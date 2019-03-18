3161bc13ec3a021f1254be7274cf39d02b6357f6
Jonathan Helmus <jjhelmus@gmail.com>
2019-03-18 13:53:23 -0500

    use flags/args to select external blosc library

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,7 @@ set BZIP2_DIR=%LIBRARY_PREFIX%
 
 %PYTHON% setup.py install --hdf5=%LIBRARY_PREFIX% ^
                           --bzip2=%LIBRARY_PREFIX% ^
+                          --lzo=%LIBRARY_PREFIX% ^
+                          --blosc=%LIBRARY_PREFIX% ^
                           --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
