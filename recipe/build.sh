8cdcead794a6cec3b483f67ba032e84308296abb
Kai Tietz <ktietz@anaconda.com>
2022-01-05 18:32:12 +0100

    prepare for python 3.10

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,7 @@ export HDF5_DIR=$PREFIX
 export LZO_DIR=$PREFIX
 export BLOSC_DIR=$PREFIX
 
+# Remove the pre-cythonized files which may not be compatible.
+rm -f tables/*.c
+
 $PYTHON -m pip install --no-deps --no-cache-dir --ignore-installed .
