ca4035bf9e56383b32c921a199a402cceb1e49ce
Steven <scrass@anaconda.com>
2023-04-11 13:21:19 -0600

    update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,11 @@ export BZIP2_DIR=$PREFIX
 export HDF5_DIR=$PREFIX
 export LZO_DIR=$PREFIX
 export BLOSC_DIR=$PREFIX
+export BLOSC2_DIR=$PREFIX
+export COPY_DLLS=FALSE
+export BLOSC2_LIBDIR=$PREFIX/lib
+export BLOSC2_INCDIR=$PREFIX/include
+export PYTABLES_NO_BLOSC2_WHEEL
 
 # Remove the pre-cythonized files which may not be compatible.
 rm -f tables/*.c
